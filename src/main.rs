983dd7f7050b33fffc6a723bc662a7a3fb0e16b1
956zs <kupp59371@gmail.com>
2024-11-13 21:06:08 +0800

    done

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("this is the first function!");
+
+    the_sec_function(); // call the second function
+}
+
+fn the_sec_function() {
+    println!("this is the second function!");
+}
