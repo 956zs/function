e3c1d87ca22a923fecc1b0aa130a8224aa2752a0
956zs <kupp59371@gmail.com>
2024-11-13 23:38:14 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+use std::io;
+
 fn main() {
     println!("this is the first function!");
 
     the_sec_function(); // call the second function
+    println!("input a number:");
+    let mut x = String::new();
+        .read_line(&mut x).expect("failed to read line");
+    the_third_function(x);
 }
 
 fn the_sec_function() {
     println!("this is the second function!");
 }
+
